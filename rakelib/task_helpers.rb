3d9f537e50aa0ebac8f6b6ac07b9ace1f1510e8e
Rob Wise <robert.wise@outlook.com>
2015-11-10 01:19:15 -0500

    wip: use --no-deployment flag when bundle installing

--- rakelib/task_helpers.rb
+++ rakelib/task_helpers.rb
@@ -24,7 +24,7 @@ module ReactOnRails
     # which screws everything up since we have multiple Gemfiles in nested
     # directories.
     def bundle_install_in(dir)
-      sh_in_dir(dir, "bundle install --gemfile=Gemfile --path=$GEM_HOME  --no-frozen")
+      sh_in_dir(dir, "bundle install --gemfile=Gemfile --path=$GEM_HOME  --no-frozen  --no-deployment")
     end
 
     # Runs bundle exec using that directory's Gemfile
