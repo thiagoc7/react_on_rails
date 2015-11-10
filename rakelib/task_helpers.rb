da2156aa8ff927f8ea2eb8908399d0df6950d9aa
Rob Wise <robert.wise@outlook.com>
2015-11-10 01:57:00 -0500

    wip: remove travis bundle  config

--- rakelib/task_helpers.rb
+++ rakelib/task_helpers.rb
@@ -25,6 +25,7 @@ module ReactOnRails
     # directories.
     def bundle_install_in(dir)
       sh_in_dir(dir, "bundle install --gemfile=Gemfile --no-deployment")
+      # sh_in_dir(dir, "bundle install --gemfile=Gemfile --no-deployment")
       # sh_in_dir(dir, "bundle install --gemfile=Gemfile --path=$GEM_HOME  --no-frozen  --no-deployment")
     end
 
